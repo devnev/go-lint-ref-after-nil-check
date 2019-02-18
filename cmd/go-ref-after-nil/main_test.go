a04787697226f0cd913dedf6e76f54b620482b26
Mark Nevill <mark.nevill@gmail.com>
2019-02-18 09:58:06 +0000

    fix test

--- cmd/go-ref-after-nil/main_test.go
+++ cmd/go-ref-after-nil/main_test.go
@@ -15,19 +15,12 @@ Reference after nil check at ../../testdata/bad_print_nil_err.go:10:14
 	fmt.Println(err)
 Reference after nil check at ../../testdata/bad_return_nil_err.go:8:9
 	return err
-exit status 1
 `
 	cmd := exec.Command("go", "run", "main.go", "../../testdata")
 	out, err := cmd.CombinedOutput()
 	t.Log("output:\n", string(out))
-	if err == nil {
-		t.Errorf("expected check command %q to fail, but got success", cmd.Args)
-	} else if exitErr, ok := err.(*exec.ExitError); !ok {
-		t.Errorf("failed to run %q: %s", cmd.Args, err.Error())
-	} else if ws, ok := exitErr.Sys().(syscall.WaitStatus); !ok {
-		t.Errorf("unable to determine exit status of %q", cmd.Args)
-	} else if ws.ExitStatus() != 1 {
-		t.Errorf("expected %q to have exit status 1, got %d", cmd.Args, ws.ExitStatus())
+	if err != nil {
+		t.Errorf("expected check command %q to succeed, got %s", cmd.Args, err.Error())
 	}
 	testOutput(t, expectedOutput, string(out))
 }
@@ -37,9 +30,9 @@ func TestCmd_machineOutput(t *testing.T) {
 ../../testdata/bad_conditional_print_nil_err.go:12:15
 ../../testdata/bad_print_nil_err.go:10:14
 ../../testdata/bad_return_nil_err.go:8:9
-exit status 1
+exit status 2
 `
-	cmd := exec.Command("go", "run", "main.go", "-machine", "../../testdata")
+	cmd := exec.Command("go", "run", "main.go", "-machine", "-exit-code", "../../testdata")
 	out, err := cmd.CombinedOutput()
 	t.Log("output:\n", string(out))
 	if err == nil {
@@ -49,6 +42,7 @@ exit status 1
 	} else if ws, ok := exitErr.Sys().(syscall.WaitStatus); !ok {
 		t.Errorf("unable to determine exit status of %q", cmd.Args)
 	} else if ws.ExitStatus() != 1 {
+		// note that this is checking `go run`'s exit status, not the linters. the linter's exit status is in the output.
 		t.Errorf("expected %q to have exit status 1, got %d", cmd.Args, ws.ExitStatus())
 	}
 	testOutput(t, expectedOutput, string(out))
